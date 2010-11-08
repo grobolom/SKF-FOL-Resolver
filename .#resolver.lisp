b5ed02b33320f8b9d356b43299303c44889d4010
Vasja Volin <vasja@superprofundo.com>
2010-11-07 23:47:05 -0500

    Finished build-substitution function, compiled, did some serious work

---
+++ .#resolver.lisp
@@ -0,0 +1,1 @@
+grobolom@grobolom-P55A-UD3.7582:1289169834
--- resolver.lisp
+++ resolver.lisp
@@ -5,23 +5,24 @@
 ;; terms are 'imaginary' constructs - if strict definition is necessary, then
 ;; we will create a struct for them that has two variables : type and value
 
-(defstruct (stmt)
-  "An Atomic Sentence - Predicate + Terms" 
-  pred terms)
 
-(defstruct (func)
-  "A Function - this is similar to an atomic sentence"
-  pred terms)
+;; An Atomic Sentence - Predicate + Terms
+(defstruct (stmt) pred terms)
 
-(defstruct (skfunc)
-  "This is a Skolem Function - I expect this to have slightly different functionality
-   than the standard function, so it is a different struct. EX: F(x)"
-  pred vars)
 
-(defstruct (var)
-  "A variable - it has a default value of nil, but if can be assigned
-   the value of a constant"
-  (value nil))
+;; A Function - this is similar to an atomic sentence
+(defstruct (func) pred terms)
+
+
+;;  This is a Skolem Function - I expect this to have slightly different functionality
+;;  than the standard function, so it is a different struct. EX: F(x)
+(defstruct (skfunc) pred vars)
+
+
+;; A variable - it has a default value of nil, but if can be assigned
+;; the value of a constant
+(defstruct (var) (value nil))
+
 
 ;; Defining a complex sentence here
 
@@ -30,9 +31,10 @@
 ;; but since we are assumed to be in SNF, we only need to worry about
 ;; the NOT symbol, or ! - this is because SNF sentences must be split
 ;; by ORS, and complex sentences are always split by ANDS.
+(defstruct (sentence) statements)
 
-(defstruct (sentence)
-  statements)
+;; Defining a substitution as below
+(defstruct (substitution) vars values)
 
 ;; Okay.. so now the Unification function?
 
@@ -43,9 +45,17 @@
 ;; +theta+ that is a set of {x1/y1 .. xn/yn} substitutions that fits the two
 ;; clauses X and Y
 
+(defun build-substitution (A B) ;Takes two clauses
+  (let ((a-terms (stmt-terms A))
+	(b-terms (stmt-terms B)))
+    (print a-terms)
+    (print b-terms)
+    t))
+
 (defun get-substitution (A B) ; These are Clauses AKA atomic sentences AKA stmt
   (if (and (stmt-p A) ; if A is a statement
 	   (stmt-p B) ; and B is too
 	   (eq (stmt-pred A) (stmt-pred B))) ;and the predicates are equal
       (build-substitution A B) ; build the substitution
       nil))
+
