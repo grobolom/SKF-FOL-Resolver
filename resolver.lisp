4af0e173aec5f0e465c6bf2ff61c5c483115f3fb
Vasja Volin <vasja@superprofundo.com>
2010-11-11 01:41:05 -0500

    Created and fixed subsitution (subs) function Returned compound function to standard function call form Next Step: 	Convert Predicates to [.Function] form?

--- resolver.lisp
+++ resolver.lisp
@@ -1,7 +1,7 @@
 ;; Defining some basic types : an atomic sentence, and what it is built of
 
 (defstruct (compound
-	     (:conc-name nil)
+	     ;(:conc-name nil)
 	     (:print-function
 	      (lambda (struct stream depth)
 		(declare (ignore depth))
@@ -11,29 +11,50 @@
 	     op args)
 
 (defun unify (x y +theta+)
-  "returns a substitution to make x and y identical"
-  (print "====UNIFY====")(print x)(print y)(print +theta+)
   (cond ((eq +theta+ 'failure) 'failure)
 	((eql x y) +theta+)
 	((var? x) (unify-var x y +theta+))
 	((var? y) (unify-var y x +theta+))
 	((and (compound-p x) (compound-p y))
-	 (unify (args x) (args y)
-		(unify (op x) (op y) +theta+)))
+	 (unify (compound-args x) (compound-args y)
+		(unify (compound-op x) (compound-op y) +theta+)))
 	((and (listp x) (listp y))
 	 (unify (cdr x) (cdr y)
 		(unify (car x) (car y) +theta+)))
 	(t 'failure)))
 
 (defun unify-var (var x +theta+)
-  "returns a substitution"
-  (print "====VARR====")(print var)(print x)(print +theta+)
-; need some association code here
-; we're going to have a data structure : list with two elements
-; ( variable value ) that represents {var/val}
   (cond ((assoc var +theta+) (unify (cadr (assoc var +theta+)) x +theta+))
 	((assoc x +theta+) (unify var (cadr (assoc x +theta+)) +theta+))
 	(t (cons (list var x) +theta+))))
 
 (defun var? (a)
     (when (symbolp a) (eq (char (string a) 0) #\?)))
+
+;(defun substite (clause +theta+)
+;  "substitutes variables in the clause with the substitution"
+; (let ((sub (cadr (assoc clause +theta+))))
+;   (cond ((compound-p clause) ; Compound? Keep the name, go inside to substitute.
+;	   (make-compound :op (compound-op clause)
+;			  :args (substite (compound-args clause) +theta+)))
+;	  ((var? clause) (cond ((and sub(compound-p sub))
+;				(substite sub +theta+))
+;			       (sub sub)
+;			       (t clause)))
+;	  ((listp clause) (cons (substite (car clause) +theta+) (substite (cdr clause) +theta+)))
+;	  (t clause))))
+
+(defun subs (clause +theta+)
+  (print "=====SUBS=====")(print clause)(print +theta+)
+  (cond ((null clause) nil)
+	((compound-p clause)
+	 (make-compound :op (compound-op clause)
+			:args (subs (compound-args clause) +theta+)))
+	((var? clause)
+	 (let ((sub (cadr (assoc clause +theta+))))
+	   (cond ((and sub (compound-p sub))
+		  (subs sub +theta+))
+		 (sub sub)
+		 (t clause))))
+	((listp clause)(cons (subs (car clause) +theta+) (subs (cdr clause) +theta+)))
+	(t clause)))
