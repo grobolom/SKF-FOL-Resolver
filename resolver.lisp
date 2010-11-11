8fa1edd36cd0c4eb92e875bcd02aa192ccd26712
Vasja Volin <vasja@superprofundo.com>
2010-11-10 22:24:50 -0500

    Removed Unnecessary Code\nFixed up function formatting\nChanged compound clause function calls

--- resolver.lisp
+++ resolver.lisp
@@ -1,93 +1,24 @@
 ;; Defining some basic types : an atomic sentence, and what it is built of
 
-;; statements, functions, and variables are represented as structs
-;; constants will simply be represented with atoms
-;; terms are 'imaginary' constructs - if strict definition is necessary, then
-;; we will create a struct for them that has two variables : type and value
-
-
-;; An Atomic Sentence - Predicate + Terms
-
-;!!! Might Not Be Necessary
-;(defstruct (stmt) pred terms)
-
-
-;; A Function - this is similar to an atomic sentence
-
-;!!! Might Not Be Necessary
-;(defstruct (func) pred terms)
-
-
-;; This is now any compound statement : King(x) | Knows(y,Mother(y))
 (defstruct (compound
+	     (:conc-name nil)
 	     (:print-function
 	      (lambda (struct stream depth)
 		(declare (ignore depth))
-		(format stream "~A(~A)"
+		(format stream "~A~A"
 			(compound-op struct)
 			(compound-args struct)))))
 	     op args)
 
-
-;; A variable - it has a default value of nil, but if can be assigned
-;; the value of a constant
-(defstruct (u-var
-	     (:print-function
-	      (lambda (struct stream depth)
-		(declare (ignore depth))
-		(format stream "~A"
-			(u-var-name struct)))))
-	     name)
-
-;; Defining a complex sentence here
-
-;; It consists of atomic sentences separated by connectives
-;; connectives are normally !, =>, /\, \/, <=>
-;; but since we are assumed to be in SNF, we only need to worry about
-;; the NOT symbol, or ! - this is because SNF sentences must be split
-;; by ORS, and complex sentences are always split by ANDS.
-;(defstruct (sentence) statements)
-
-;; Defining a substitution as below
-;(defstruct (substitution) vars values)
-
-;; Okay.. so now the Unification function?
-
-;; This function will take two complex sentences (struct sentence)
-;; it will output the combined statement with the proper substitutions
-
-;; We first need a substitution finder function that returns the substitution
-;; +theta+ that is a set of {x1/y1 .. xn/yn} substitutions that fits the two
-;; clauses X and Y
-
-;(defun build-substitution (A B) ;Takes two clauses
-;  (let ((a-terms (stmt-terms A))
-;	(b-terms (stmt-terms B)))
-;    (print a-terms)
-;    (print b-terms)
-;    t))
-
-;(defun get-substitution (A B) ; These are Clauses AKA atomic sentences AKA stmt
-;  (if (and (stmt-p A) ; if A is a statement
-;	   (stmt-p B) ; and B is too
-;	   (eq (stmt-pred A) (stmt-pred B))) ;and the predicates are equal
-;      (build-substitution A B) ; build the substitution
-;      nil))
-
-;; Commented out This bullshit so I can build according to the book's pseudocode.
-;; May require some modification of our structs.
-;; Guess we'll see.
-
 (defun unify (x y +theta+)
   "returns a substitution to make x and y identical"
-  (print "====UNIFY====")(print x)(print y)(print +theta+)
   (cond ((eq +theta+ 'failure) 'failure)
 	((eql x y) +theta+)
 	((var? x) (unify-var x y +theta+))
 	((var? y) (unify-var y x +theta+))
 	((and (compound-p x) (compound-p y))
-	 (unify (compound-args x) (compound-args y)
-		(unify (compound-op x) (compound-op y) +theta+)))
+	 (unify (args x) (args y)
+		(unify (op x) (op y) +theta+)))
 	((and (listp x) (listp y))
 	 (unify (cdr x) (cdr y)
 		(unify (car x) (car y) +theta+)))
@@ -95,10 +26,6 @@
 
 (defun unify-var (var x +theta+)
   "returns a substitution"
-  (print "====VARR====")(print var)(print x)(print +theta+)
-; need some association code here
-; we're going to have a data structure : list with two elements
-; ( variable value ) that represents {var/val}
   (cond ((assoc var +theta+) (unify (cadr (assoc var +theta+)) x +theta+))
 	((assoc x +theta+) (unify var (cadr (assoc x +theta+)) +theta+))
 	(t (cons (list var x) +theta+))))
