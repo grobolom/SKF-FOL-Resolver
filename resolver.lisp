81c3dbd7c7f0c766bcd40fa86330231bb3b4131a
Vasja Volin <vasja@superprofundo.com>
2010-11-18 00:11:54 -0500

    Finished reslv function DAMN THING WORKS! Resolution works correctly!

--- resolver.lisp
+++ resolver.lisp
@@ -8,20 +8,9 @@
 		(format stream "~A~A"
 			(compound-op struct)
 			(compound-args struct)))))
-	     op &rest args)
+  op &rest args)
 
-; defun m-c (a &rest b)
-; defun atp (kb a)
-; defun resolve (skb depth)
-; defun get-next-res (node fringe)
-; defun r-s (x y)
-; defun unify-facts (x y)
-; defun unify-clauses (x y)
-; defun unify (x y &optional +theta+)
-; defun unify-var (var x +theta+)
-; defun subs (clause +theta+)
-; defun sd-apart (KB)
-; defun var-replace (stmt i)
+;(defstruct compound op args)
 
 (defun m-c (a &rest b)
   (make-compound :op a :args b))
@@ -30,59 +19,24 @@
   (let ((skb (sd-apart kb)))
     (resolve (append skb (list a)) 20)))
 
-(defun resolve (skb depth)
-  (format t "~%~%~A~%~A~%~A~%~A~%~A~%~A~%~A~%~A~%~A"
-	  "==RESOLVE==" (nth 0 skb)(nth 1 skb)
-	  (nth 2 skb) (nth 3 skb) (nth 4 skb)
-	  (nth 5 skb) (nth 6 skb) (nth 7 skb))
-  (let ((node (car skb)) ;current node to try matching
-	(fringe (cdr skb)) ;the rest of the list
-	(next-res (get-next-res (car skb)(cdr skb)))) ;the node to resolve
-    (cond ((eql depth 0) nil)
-	  ((not (null next-res))
-	   (resolve (append (remove next-res fringe)
-			    (list (r-s node next-res))) (1- depth)))
-	  (t (resolve (append fringe (list node)) (1- depth))))))
-
 (defun reslv (nres &optional pres depth)
-  (let ((node (car (append nres pres)))
-;	(fringe (append pres nres))
-	(nextr (get-next-res (car (append nres pres))
-			     (cdr (append pres nres)))))
-;    (print (last pres))(print "=Reslv")(print node)(print nextr)
+  (let ((node (car (last pres)))
+	(nextr (get-next-res (car (last pres)) (append nres pres))))
     (format t "~%~A~%~A~%~A~%~%~A~%~%~A~%"
-	    "=Resolve" node nextr nres pres)
+	    "=Resolve" node nextr nres pres) 
     (cond ((eql depth 0) nil)
 	  ((not (null nextr))
-	   (reslv (remove nextr (cdr nres))
-		  (append (remove node pres) (list (r-s node nextr)))
+	   (reslv (sort (append nres pres) #'(lambda (x y) (< (list-length x) (list-length y))))
+		  (list (r-s node nextr))
 		  (1- depth)))
-	  (t
-	   (reslv (cdr nres)
-		  (append (cdr pres) (list (car pres)))
+	  (t (print "BALLS")
+	   (reslv nres
+		  (append (cdr pres)(list (car pres)))
 		  (1- depth))))))
 
 (defun get-next-res (node fringe)
   (loop for e in fringe
      do (unless (equal (unify-facts e node) 'failure)(return e))))
-
-;(defun r-s (x y)
-;  (format t "~%~A~%" "==Resolve-Sentences==")
-;  (let ((tset (append x y))
-;	(sset (unify-facts x y)))
-;    (loop for e in tset
-;       do (format t "~%~%~A ~A ~A" restt (find e x) (find e y))
-;       do (format t "~%~A ~A" (unify-facts (list e) x) (unify-facts (list e) y))
-;       collect (cond ((find e x)
-;		      (cond ((eql (unify-facts (list e) y) '(+NULL+)) 'nil)
-;			    ((eql (unify-facts (list e) y) 'nil) (subs e sset))
-;			    (t 'nil)))
-;		     ((find e y)
-;		      (cond ((eql (unify-facts (list e) x) '(+NULL+)) 'nil)
-;			    ((eql (unify-facts (list e) x) 'nil) (subs e sset))
-;			    (t 'nil))))
-;       into restt
-;       finally (return (remove-if 'null restt)))))
 
 (defun r-s (x y)
 ;  (format t "~%=R-S=~%~A ~A" x y)
@@ -216,6 +170,7 @@
 ; (setf p (list (m-c '!Kills 'Curiosity 'Tuna)))
 
 ; (setf KB (list f g e p b d a c))
+; (setf skb (sd-apart kb))
 
 ; (setf SAB (list (m-c '!Loves '?x2 (m-c 'F '?x2)) (m-c 'Loves (m-c 'G '?x2) '?x2)))
 ; (setf SAD (list (m-c '!Animal '?x4) (m-c 'Loves 'Jack '?x4)))
